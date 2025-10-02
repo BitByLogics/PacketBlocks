1315259d1984b94c4efa55a9d8fbd01fdfdeefa5
Justin <33465177+BitByLogics@users.noreply.github.com>
2025-10-02 16:50:13 -0400

    Wrap `manager.getBlocks` result in a new `ArrayList` to ensure mutability.

--- src/main/java/net/bitbylogic/packetblocks/adapter/ChunkLoadAdapter.java
+++ src/main/java/net/bitbylogic/packetblocks/adapter/ChunkLoadAdapter.java
@@ -14,6 +14,7 @@ import org.bukkit.Location;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -31,7 +32,7 @@ public class ChunkLoadAdapter implements PacketListener {
         int chunkX = packet.getColumn().getX();
         int chunkZ = packet.getColumn().getZ();
 
-        List<PacketBlock> blocks = manager.getBlocks(player.getWorld(), chunkX, chunkZ);
+        List<PacketBlock> blocks = new ArrayList<>(manager.getBlocks(player.getWorld(), chunkX, chunkZ));
         if (blocks.isEmpty()) return;
 
         BaseChunk[] sections = packet.getColumn().getChunks();
