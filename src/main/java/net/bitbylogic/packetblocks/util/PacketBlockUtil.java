59a8a566d1db1d276e0e9fadad469703b975c51d
Justin <33465177+BitByLogics@users.noreply.github.com>
2025-10-02 22:46:16 -0400

    Refactor ray-tracing logic to integrate vanilla ray-tracing results and improve accuracy.

--- src/main/java/net/bitbylogic/packetblocks/util/PacketBlockUtil.java
+++ src/main/java/net/bitbylogic/packetblocks/util/PacketBlockUtil.java
@@ -3,6 +3,7 @@ package net.bitbylogic.packetblocks.util;
 import lombok.NonNull;
 import net.bitbylogic.packetblocks.PacketBlocks;
 import net.bitbylogic.packetblocks.block.PacketBlockManager;
+import org.bukkit.FluidCollisionMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -47,38 +48,34 @@ public class PacketBlockUtil {
         Location eye = player.getEyeLocation();
         Vector direction = eye.getDirection().normalize();
 
-        Vector current = eye.toVector();
         World world = player.getWorld();
 
-        double step = 0.1;
-        double eps = 1e-6;
+        RayTraceResult vanillaResult = world.rayTraceBlocks(eye, direction, range, FluidCollisionMode.NEVER, false);
+
+        Vector current = eye.toVector();
+        double step = 0.05;
 
         for (double traveled = 0; traveled <= range; traveled += step) {
             current.add(direction.clone().multiply(step));
             Block block = world.getBlockAt(current.getBlockX(), current.getBlockY(), current.getBlockZ());
 
-            BoundingBox pointBox = new BoundingBox(
-                    current.getX() - eps, current.getY() - eps, current.getZ() - eps,
-                    current.getX() + eps, current.getY() + eps, current.getZ() + eps
-            );
-
             if (PacketBlockManager.getBlock(block.getLocation()).isPresent()) {
                 Material blockMaterial = PacketBlockManager.getBlock(block.getLocation()).get().getBlockData().getMaterial();
                 BoundingBox boundingBox = BoundingBoxes.getBoxAt(blockMaterial, block.getLocation());
 
-                if(boundingBox == null || !boundingBox.overlaps(pointBox)) {
+                if(boundingBox == null) {
                     continue;
                 }
 
-                return new RayTraceResult(current, block, null);
-            }
+                RayTraceResult boxResult = boundingBox.rayTrace(eye.toVector(), direction, range);
 
-            if (!block.isEmpty()) {
-                return new RayTraceResult(current, block, null);
+                if (boxResult != null) {
+                    return new RayTraceResult(boxResult.getHitPosition(), block, boxResult.getHitBlockFace());
+                }
             }
         }
 
-        return null;
+        return vanillaResult;
     }
 
 }
